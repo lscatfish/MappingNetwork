d82c224ca10083c4a0189d2f2152cf2454b8f81f
lscatfish <2561925435@qq.com>
2026-07-20 01:05:06 +0800

    feat: add mapping.generator.MLP prebuilt block

--- mapping/generator/__init__.py
+++ mapping/generator/__init__.py
@@ -2,5 +2,6 @@ from mapping.generator.block import Block
 from mapping.generator.linear import Linear
 from mapping.generator.conv import Conv1d, Conv2d
 from mapping.generator.lrd import LRDLayer
+from mapping.generator.mlp import MLP
 
-__all__ = ['Block', 'Linear', 'Conv1d', 'Conv2d', 'LRDLayer']
+__all__ = ['Block', 'Linear', 'Conv1d', 'Conv2d', 'LRDLayer', 'MLP']
